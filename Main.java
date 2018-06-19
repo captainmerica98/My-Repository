5ab5e4293757d0931ae7804cb9a55177775983d0
captainmerica98 <36421616+captainmerica98@users.noreply.github.com>
2018-06-19 17:47:54 -0400

    cowsay thing

---
+++ Main.java
@@ -0,0 +1,54 @@
+public class Main
+{
+    public static void main(String args[])
+            throws InterruptedException
+    {
+        System.out.println("Hello World!");
+        System.out.println(.6f/.7f);
+        int num = 1;
+      //  while(num > 0) {
+      //      System.out.println(num);
+      //      num++;
+
+            System.out.println(" ______________________________\n" +
+                    "    < WAAASSSSAAAAPPPPPPPP >\n" +
+                    " ------------------------------\n" +
+                    "        \\   ^__^\n" +
+                    "         \\  (oo)\\_______\n" +
+                    "            (__)\\       )\\/\\\n" +
+                    "                ||----w |\n" +
+                    "                ||     ||");
+       // }
+        System.out.println("\\");
+        Thread.sleep(300);
+        System.out.println(" \\");
+        Thread.sleep(300);
+        System.out.println("  \\");
+        Thread.sleep(100);
+        System.out.print("W");
+        Thread.sleep(100);
+        System.out.print("A");
+        Thread.sleep(100);
+        System.out.print("A");
+        Thread.sleep(100);
+        System.out.print("S");
+        Thread.sleep(100);
+        System.out.print("S");
+        Thread.sleep(100);
+        System.out.print("S");
+        Thread.sleep(100);
+        System.out.print("U");
+        Thread.sleep(100);
+        System.out.print("U");
+        Thread.sleep(100);
+        System.out.print("U");
+        Thread.sleep(100);
+        System.out.print("P");
+        Thread.sleep(100);
+        System.out.print("P");
+        Thread.sleep(100);
+        System.out.print("P");
+
+
+    }
+}
